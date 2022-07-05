2710728ed11c959ee4bfc38245fe5c44028507e5
Ida Delphine <idadelm@gmail.com>
2022-07-05 11:33:22 +0100

    Created routes

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID string "json: 'id'"
+	Isbn string "json: 'isbn'"
+	Title string "json: 'title'"
+	Director *Director "json: 'director'"
+}
+
+type Director struct {
+	FirstName string "json: 'firstname'"
+	LastName string "json: 'lastname'"
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "48007", Title: "Movie One", Director: &Director{FirstName: "John", LastName: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "48677", Title: "Movie Two", Director: &Director{FirstName: "Jane", LastName: "Doe"}})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8080\n")
+	log.Fatal(http.ListenAndServe(":8080", r))
+
+}
